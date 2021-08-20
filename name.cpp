c3c58269ef3209bdfdfafdb7c550907b63ba2c28
isonguyom <isonguyom@gmail.com>
2021-08-20 11:24:14 +0100

    first commit

---
+++ name.cpp
@@ -0,0 +1,13 @@
+//Program to print my name with c++
+#include <iostream>
+using namespace std;
+
+int main() {
+//	Declarations
+string fName, lName;
+fName = "Martin";
+lName = "Isonguyo";
+  cout << "Hello World!" << endl;
+  cout << "I'm " + fName + " " + lName << endl;
+  return 0;
+}
